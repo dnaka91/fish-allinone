9aed06ebb6e0ddd49cd835ee8dd0ababa637a653
Dominik Nakamura <dnaka91@gmail.com>
2020-09-11 13:42:01 +0900

    Add custom color theme

---
+++ conf.d/colors.fish
@@ -0,0 +1,23 @@
+set -g fish_color_autosuggestion    white -d
+set -g fish_color_cancel            -r
+set -g fish_color_command           magenta
+set -g fish_color_comment           brred -d
+set -g fish_color_cwd               green
+set -g fish_color_cwd_root          red
+set -g fish_color_end               brwhite -o
+set -g fish_color_error             red -o
+set -g fish_color_escape            brgreen
+set -g fish_color_history_current   -o
+set -g fish_color_host              normal
+set -g fish_color_host_remote       yellow
+set -g fish_color_match             -b brblue
+set -g fish_color_normal            normal
+set -g fish_color_operator          yellow
+set -g fish_color_param             blue
+set -g fish_color_quote             green
+set -g fish_color_redirection       00afff
+set -g fish_color_search_match      bryellow -b brblack
+set -g fish_color_selection         white -o -b brblack
+set -g fish_color_status            red
+set -g fish_color_user              brgreen
+set -g fish_color_valid_path        -u
